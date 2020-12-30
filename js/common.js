b6fa562b7f9a57b78a9e6f14fcfaa9a80a0389be
woosunah <sunah5011@gmail.com>
2020-12-30 18:56:18 +0900

    instagram hover add

--- js/common.js
+++ js/common.js
@@ -48,38 +48,3 @@ $(function () {
     pager: true,
   });
 });
-
-// function gnbMenuFnc2(element) {
-//   var menuList1 = element.children;
-//   var hoverMenu = null;
-
-//   for (var i = 0; i < menuList1.length; i++) {
-//     menuList1[i].children[0].addEventListener('mouseover', gnbMenuHandler1);
-//     menuList1[i].children[0].addEventListener('click', gnbMenuHandler2);
-//   }
-
-//   function gnbMenuHandler1() {
-//     if (document.querySelector('html').className == 'pc') {
-//       if (hoverMenu != null) hoverMenu.classList.remove('on');
-//       this.classList.add('on');
-//       hoverMenu = this;
-//     }
-//   }
-//   function gnbMenuHandler2() {
-//     if (document.querySelector('html').className == 'mobile') {
-//       this.classList.toggle('on');
-//       if (this.className.indexOf('on') >= 0) {
-//         hoverMenu = this;
-//       } else {
-//         hoverMenu = null;
-//       }
-//     }
-//   }
-//   element.addEventListener('mouseleave', function () {
-//     if (hoverMenu != null) hoverMenu.classList.remove('on');
-//     hoverMenu = null;
-//   });
-// }
-// window.addEventListener('load', function () {
-//   gnbMenuFnc2(document.querySelector('#gnb'));
-// });
